1a190f908ad82cc74862da35c0e92914086a3644
Caiah Nissen <zodern@users.noreply.github.com>
2017-11-24 22:09:26 -0600

    Fix upload percentage

--- src/upload.js
+++ src/upload.js
@@ -19,17 +19,17 @@ export default function upload(appConfig, bucket, key, bundlePath) {
     const uploader = s3.upload(params);
 
     uploader.on('httpUploadProgress', (progress) => {
-      const percentage = Math.floor(progress.loaded / progress.total) * 100;
+      const percentage = Math.floor(progress.loaded / progress.total * 100);
 
       if (percentage !== lastPercentage) {
         console.log(`  Uploaded ${percentage}%`);
+
+        if (percentage === 100) {
+          console.log('  Finishing upload. This could take a couple minutes');
+        }
       }
 
       lastPercentage = percentage;
-
-      if (percentage === 100) {
-        console.log('  Finishing upload. This could take a couple minutes');
-      }
     });
 
     uploader.send((err, result) => {
