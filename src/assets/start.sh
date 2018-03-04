1b0c48f1d0954f4ae11960add6288643266807ad
Unknown <zodern@users.noreply.github.com>
2018-03-03 21:10:58 -0600

    Fix using node version installed by nvm

--- src/assets/start.sh
+++ src/assets/start.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-export METEOR_SETTINGS=$(node -e 'console.log(decodeURIComponent(process.env.METEOR_SETTINGS_ENCODED))')
+# When nvm is installed, $HOME isn't set
+# resulting in nvm installed /.nvm
+export NVM_DIR="/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" >/dev/null 2>&1
 
-# Use nvm
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+nvm use default --delete-prefix --silent
 
 echo "Node version"
 echo $(node --version)
 echo "Npm version"
 echo $(npm --version)
+
+export METEOR_SETTINGS=$(node -e 'console.log(decodeURIComponent(process.env.METEOR_SETTINGS_ENCODED))')
 
 echo "=> Starting health check server"
 node health-check.js &
