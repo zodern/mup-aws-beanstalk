052755e4e0e381f62cfb8b717a33597e79c14af4
jamauro <jamauro@gmail.com>
2021-08-03 21:21:20 -0500

    Update for Node 14 and Meteor 2.3+

--- src/command-handlers.js
+++ src/command-handlers.js
@@ -32,6 +32,7 @@ import {
   ensurePoliciesAttached,
   ensureBucketPolicyAttached,
   getAccountId,
+  getNodeVersion,
   getLogs,
   logStep,
   names,
@@ -477,7 +478,12 @@ export async function reconfig(api) {
     const {
       SolutionStacks
     } = await beanstalk.listAvailableSolutionStacks().promise();
-    const solutionStack = SolutionStacks.find(name => name.endsWith('running Node.js'));
+    const bundlePath = config.app.buildOptions.buildLocation;
+    const {
+      nodeVersion
+    } = getNodeVersion(api, bundlePath);
+    const majorNodeVersion = nodeVersion.split('.')[0];
+    const solutionStack = SolutionStacks.find(name => name.endsWith(`running Node.js ${majorNodeVersion}`));
 
     const [version] = await ebVersions(api);
     await beanstalk.createEnvironment({
